2a2621eb0fc60706a83c61605b3bd18d52f42cad
serp-ya <sergei.kundryukov1989@gmail.com>
2017-07-14 00:47:31 +0300

    Add first grid

--- game.js
+++ game.js
@@ -220,3 +220,10 @@ class LevelParser {
         return undefined;
     }
 }
+
+const grid = [
+    new Array(3),
+    ['wall', 'wall', 'lava']
+];
+const level = new Level(grid);
+runLevel(level, DOMDisplay);
